22da9a65bcdc64f486e5d1b0614a1e9b66ea1bb3
DongRyeong Lee <dr11291129@gmail.com>
2024-11-06 15:26:05 +0900

    feat: Recoil 설치, 프로젝트 적용 #3
    - 전역 상태관리를 위한 Recoil을 설치
    - Recoil 테스트를 위한 stateCounter.tsx 작성
    - stateCounter 동작 여부 확인을 위한 Home.tsx 일부 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useRecoilState } from "recoil";
+import { stateCouonter } from "../../stores/stateCounter";
 
 const Home = () => {
-  return <div>This is the Home page</div>;
+  const [count, setCount] = useRecoilState(stateCouonter);
+
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+      <button onClick={() => setCount(count + 1)}>Increment {count}</button>
+    </div>
+  );
 };
 
 export default Home;
