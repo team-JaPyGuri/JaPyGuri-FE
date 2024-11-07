554b90ebf05de04c12a3d189ff8001c1fc5c3b25
DongRyeong Lee <dr11291129@gmail.com>
2024-11-07 17:11:34 +0900

    feat: Button component 작성 #9
    - Button component 코드 작성
    - Button component 확인을 위한 Home.tsx 일부 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Header from "../Header";
 import NavigationBar from "../NavigationBar/inex";
 import Footer from "../../Footer";
+import Button from "../Button";
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,7 +13,12 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="w-full min-w-[20rem] max-w-[37.5rem] min-h-[100dvh] bg-grayscale-200 flex-col sc:border-x sc:border-opacity-50 sc:border-solid sc:border-grayscale-300 sc:fixed sc:left-1/2 sc:transform sc:-translate-x-1/2">
       <Header />
       <NavigationBar />
-      <main className="w-full">{children}</main>
+      <main className="w-full">
+        {children}
+        <div className="w-full p-4">
+          <Button>테스트</Button>
+        </div>
+      </main>
       <Footer />
     </div>
   );
