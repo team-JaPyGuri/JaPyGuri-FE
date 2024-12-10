1dea5d36fa5155f12e590d779590f23b85e1f6e7
DongRyeong Lee <dr11291129@gmail.com>
2024-12-10 15:45:27 +0900

    chore: SocketInitializer tryon_result 타입 변경

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3b54rc282ko"
+    "revision": "0.36p0ui5p2p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
