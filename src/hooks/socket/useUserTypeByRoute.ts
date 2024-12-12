284553d70ec5aedbdcb24efbd572a105ba7654fa
DongRyeong Lee <dr11291129@gmail.com>
2024-12-12 18:58:50 +0900

    feat: SocketInitializer 선언 위치 변경 #22
    - Socket 전역 관리를 통해 단일 연결 시도를 유지하기 위해 위치 변경

--- src/hooks/socket/useUserTypeByRoute.ts
+++ src/hooks/socket/useUserTypeByRoute.ts
@@ -1,22 +1,23 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { stateUser } from "../../stores/stateUser";
 
 const useUserTypeByRoute = () => {
   const location = useLocation();
-  const user = useRecoilState(stateUser);
+  const user = useRecoilValue(stateUser);
   const setUser = useSetRecoilState(stateUser);
 
   useEffect(() => {
     if (location.pathname === "/dashboard") {
       setUser({ userType: "shop", userId: import.meta.env.VITE_API_SHOP_ID });
     } else {
-      if (user[0] === null || user[0].userType === "shop") {
+      if (user === null || user.userType === "shop") {
         setUser({
           userType: "customer",
           userId: import.meta.env.VITE_API_USER_ID,
         });
+        console.log(1);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
